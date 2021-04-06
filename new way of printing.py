ded85be078a05345ef423befbf5f05fe363f3738
mohan-sah <72912769+mohan-sah@users.noreply.github.com>
2021-04-06 15:14:04 +0530

    Create new way of printing.py
    i discovered an invented way of  python print

---
+++ new way of printing.py
@@ -0,0 +1,3 @@
+n = int(input("Enter an integer:"))
+quotient = int(100/n)
+print("100/{}={}".format(n ,quotient))#kage bunshi no jutsu . repalcing variables with this'{}' brackets and order wise putting its
